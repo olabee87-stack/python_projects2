9f16e4b5f8dd189feb037ed0ea5b1212460363a8
Olabisi Odusanya <odus.bisi07@gmail.com>
2022-03-31 14:43:15 +0300

    python_pro

---
+++ simple_app/if.py
@@ -0,0 +1,8 @@
+number = 0
+
+if number > 0:
+    print(f"""The number {number} is positive""")
+elif number == 0:
+    print(f"""This is zero: {number}""")
+else:
+    print(f"""The number {number} is negative...""")
