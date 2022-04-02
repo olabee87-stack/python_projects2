615304bf234f613dba2961ac460ffbd2d83a1867
Olabisi Odusanya <odus.bisi07@gmail.com>
2022-04-02 08:55:17 +0300

    python_pro

---
+++ simple_app/in.py
@@ -0,0 +1,7 @@
+age = int(input('Enter your age in years: '))
+height = int(input('Enter your height in inches: '))
+
+if age > 12 and height > 60:
+    print('Welcome aboard.')
+else:
+    print('You do not meet the requirements to ride.')
